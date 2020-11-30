9841f57084ab4b55a04d235ad8d20ca2772100cb
yash1804 <70275059+yash1804@users.noreply.github.com>
2020-11-30 22:35:46 +0530

    Add files via upload

---
+++ brick2.js
@@ -0,0 +1,19 @@
+class Brick2 {
+    constructor(x, y, width, height) {
+      var options = {
+        isStatic: false
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("blue");
+      rect(pos.x, pos.y, this.width, this.height);
+      
+    }
+  }
